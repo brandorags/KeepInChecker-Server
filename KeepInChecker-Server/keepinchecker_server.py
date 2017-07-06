ce5852e09e232882c378b2a7df81142e2f91d7f4
brandorags <brandorags@users.noreply.github.com>
2017-07-06 00:47:34 -0500

    created method for initializing server config values upon startup

--- KeepInChecker-Server/keepinchecker_server.py
+++ KeepInChecker-Server/keepinchecker_server.py
@@ -1,8 +1,44 @@
+# Copyright 2017 Brandon Ragsdale
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from configparser import ConfigParser
 from flask import Flask
 
 
+# global Flask object
 app = Flask(__name__)
+
+# global server config objects
+database_path = ''
+database_user = ''
+database_password = ''
+
+
+def init_server_config_objects():
+    global database_path
+    global database_user
+    global database_password
+
+    config = ConfigParser()
+    config.read('../server.cfg')
+
+    database_path = config['database']['DATABASE_PATH']
+    database_user = config['database']['DATABASE_USER']
+    database_password = config['database']['DATABASE_PASSWORD']
 
 
 if __name__ == '__main__':
+    init_server_config_objects()
     app.run(host='0.0.0.0')
